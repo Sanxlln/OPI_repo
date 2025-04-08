b39969ad68d94a1ea8c06eb116fbefe1a9a83327
Oleksandra <oleksandra@gmail.com>
2025-04-08 14:25:05 +0300

    Fix program on main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <random>
 
+std::random_device rd;
+std::mt19937 gen(rd());
+
 int getRandomNumber(int min, int max) {
-    std::random_device rd;
-    std::mt19937 gen(rd()); 
     std::uniform_int_distribution<> distrib(min, max);
-    
+    return distrib(gen);
+}
+
+double getRandomDouble(double min, double max) {
+    std::uniform_real_distribution<> distrib(min, max);
     return distrib(gen);
 }
 
 int main() {
-    for (int i = 0; i < 5; i++) {
+    int count = 5;  
+    std::cout << "Випадкові числа (цілі): ";
+    for (int i = 0; i < count; i++) {
         std::cout << getRandomNumber(1, 100) << " ";
     }
+
+    std::cout << "\nВипадкові числа (з плаваючою точкою): ";
+    for (int i = 0; i < count; i++) {
+        std::cout << getRandomDouble(1.0, 100.0) << " ";
+    }
+    
     std::cout << std::endl;
     return 0;
 }
